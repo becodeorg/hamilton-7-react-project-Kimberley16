46ebbeb0cec1c85ed0c458ab350d4a6d1eae8872
Kimberley 16 <kimberleypaladini@gmail.com>
2022-11-25 12:17:04 +0100

    wip api

--- src/App.js
+++ src/App.js
@@ -8,7 +8,16 @@ function App() {
   const {city, setCity} = useState("");
   const getWeather = (event) => {
     if(event.key = "enter") // le .key désigne la touche qui va être utilisée pour déclencher l'evenement
-    fetch 
+    fetch (`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+    //pour fetch, il faut toujours entouré le lien avec des ``, sinon ça ne marche pas
+    //afin de récupérer la ville qui nous intéresse, on entre ${city}
+    //le $ devant une contante est pour dire que l'on récupère une donnée entrée par l'utilisateur
+    .then( //signifie "après avoir appuyer sur enter, le programme doit faire quelque chose par la suite
+      response=> response.json //les informations récupérées seront envoyées vers un fichier nommé response.json
+    )
+    then (
+      data=>{setWeatherData(data) ; setCity("")}
+    )
   }
   return ( <div>
     <input 
