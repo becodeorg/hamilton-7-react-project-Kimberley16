e8b562780d9c2a14222ba77fb9c1eefe3b2bb0d0
Kimberley 16 <kimberleypaladini@gmail.com>
2022-11-25 11:59:18 +0100

    wip api

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import { useState } from 'react';
 
 function App() {
   const apiKey = "0e0fc87f95ad0c667bdd54d2aa2aa067";
+  const{weatherData, setWeatherData}= useState([{}]);
   const {city, setCity} = useState("");
+  const getWeather = (event) => {
+    if(event.key = "enter") // le .key désigne la touche qui va être utilisée pour déclencher l'evenement
+    fetch 
+  }
   return ( <div>
     <input 
     type="text" 
